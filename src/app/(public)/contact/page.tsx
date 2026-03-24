a064cae11d968ee6759c356454f54e6ffe001841
1viniciusdiniz <viniciusdiniz122007@gmail.com>
2026-03-24 14:49:47 -0300

    fix

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -232,7 +232,7 @@ export default function ContactPage() {
                         {/* Mapa Atualizado para Santana de Parnaíba */}
                         <div className="md:w-1/2 w-full h-[300px] md:h-[400px] rounded-3xl overflow-hidden border-8 border-white shadow-2xl relative">
                             <iframe
-                                src="http://googleusercontent.com/maps.google.com/6"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.70425785072!2d-46.9056346246736!3d-23.435056778887413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf026fdaa94a37%3A0x116e1e3343fd52e3!2sR.%20Santa%20Catarina%2C%201%20-%20Jardim%20da%20V%C3%A1rzea%2C%20Santana%20de%20Parna%C3%ADba%20-%20SP%2C%2006530-070!5e0!3m2!1spt-BR!2sbr!4v1710000000000!5m2!1spt-BR!2sbr"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
